5f9e65fe96961fe65b6fca68db9b310f80de1b9a
Matej Klotton <mklotton@cisco.com>
2016-10-03 11:44:50 +0200

    Doxygen documentation

--- test/test_vxlan.py
+++ test/test_vxlan.py
@@ -10,24 +10,28 @@ from scapy.layers.inet import IP, UDP
 from scapy_handlers.vxlan import VXLAN
 
 
-## VXLAN Test cases using bridge domain to test encapsulation and decapsulation
+## TestVxlan is a subclass of BridgeDomain, Util, VppTestCase classes.
+#
+#  TestVxlan class defines VXLAN test cases for VXLAN encapsulation,
+#  decapsulation and VXLAN tunnel termination in L2 bridge-domain.
 class TestVxlan(BridgeDomain, Util, VppTestCase):
     """ VXLAN Test Case """
 
-    ## Run __init__ for all parent class
+    ## Method to initialize all parent classes.
     #
     #  Initialize BridgeDomain objects, set documentation string for inherited
     #  tests and initialize VppTestCase object which must be called after
     #  doc strings are set.
     def __init__(self, *args):
         BridgeDomain.__init__(self)
-        self.test_decap.__func__.__doc__ = ' Decapsulate path to BD '
-        self.test_encap.__func__.__doc__ = ' Encapsulate path to BD '
+        self.test_decap.__func__.__doc__ = ' VXLAN BD decapsulation '
+        self.test_encap.__func__.__doc__ = ' VXLAN BD encapsulation '
         VppTestCase.__init__(self, *args)
 
-    ## Implementation for BridgeDomain encapsulate function.
+    ## Method for VXLAN encapsulate function.
     #
-    #  Encapsulation is done by adding original frame into new Ethernet IP UDP with VXLAN header.
+    #  Encapsulate the original payload frame by adding VXLAN header with its
+    #  UDP, IP and Ethernet fields.
     def encapsulate(self, pkt):
         return (Ether(src=self.MY_MACS[0], dst=self.VPP_MACS[0]) /
                 IP(src=self.MY_IP4S[0], dst=self.VPP_IP4S[0]) /
@@ -35,51 +39,61 @@ class TestVxlan(BridgeDomain, Util, VppTestCase):
                 VXLAN(vni=1) /
                 pkt)
 
-    ## Implementation for BridgeDomain decapsulate function.
+    ## Method for VXLAN decapsulate function.
     #
-    #  Encapsulation is done by adding original frame into new Ethernet IP UDP with VXLAN header.
+    #  Decapsulate the original payload frame by removing VXLAN header with
+    #  its UDP, IP and Ethernet fields.
     def decapsulate(self, pkt):
         return pkt[VXLAN].payload
 
-    ## Implementation for BridgeDomain check_encapsulation function.
+    ## Method for checking VXLAN encapsulation.
     #
     def check_encapsulation(self, pkt):
         # TODO: add error messages
-        ## Check Ethernet header source must be VPP's MAC and destination MY MAC which VPP knows because ARP in setup.
+        ## Verify source MAC is VPP_MAC and destination MAC is MY_MAC resolved
+        #  by VPP using ARP.
         self.assertEqual(pkt[Ether].src, self.VPP_MACS[0])
         self.assertEqual(pkt[Ether].dst, self.MY_MACS[0])
-        ## Check tunnel source(VPP IP) and destination(MY IP).
+        ## Verify VXLAN tunnel source IP is VPP_IP and destination IP is MY_IP.
         self.assertEqual(pkt[IP].src, self.VPP_IP4S[0])
         self.assertEqual(pkt[IP].dst, self.MY_IP4S[0])
-        ## Check UDP destination port, source could be arbitrary.
+        ## Verify UDP destination port is VXLAN 4789, source UDP port could be
+        #  arbitrary.
         self.assertEqual(pkt[UDP].dport, 4789)
         # TODO: checksum check
-        ## Check VNI, based on configuration must be 1.
+        ## Verify VNI, based on configuration it must be 1.
         self.assertEqual(pkt[VXLAN].vni, 1)
 
-    ## Prepare VXLAN test environment.
+    ## Class method to start the VXLAN test case.
+    #  Overrides setUpClass method in VppTestCase class.
+    #  Python try..except statement is used to ensure that the tear down of
+    #  the class will be executed even if exception is raised.
+    #  @param cls The class pointer.
     @classmethod
     def setUpClass(cls):
         super(TestVxlan, cls).setUpClass()
         try:
             ## Create 2 pg interfaces.
             cls.create_interfaces(range(2))
-            ## Configure IPv4 addressing on pg0.
+            ## Configure IPv4 addresses on VPP pg0.
             cls.config_ip4([0])
-            ## Determine MAC address for VPP's IP.
+            ## Resolve MAC address for VPP's IP address on pg0.
             cls.resolve_arp([0])
 
-            ## Create VXLAN VTEP on pg0, and put vxlan_tunnel0 and pg1 into BD.
+            ## Create VXLAN VTEP on VPP pg0, and put vxlan_tunnel0 and pg1
+            #  into BD.
             cls.api("vxlan_add_del_tunnel src %s dst %s vni 1" %
                     (cls.VPP_IP4S[0], cls.MY_IP4S[0]))
             cls.api("sw_interface_set_l2_bridge vxlan_tunnel0 bd_id 1")
             cls.api("sw_interface_set_l2_bridge pg1 bd_id 1")
         except:
-            ## In case setUpClass fails run tear down (it's not called automaticly)
+            ## In case setUpClass fails run tear down.
             cls.tearDownClass()
             raise
 
-    ## Tear down which is called after each test_* in method in this object.
+    ## Method to define VPP actions before tear down of the test case.
+    #  Overrides tearDown method in VppTestCase class.
+    #  @param self The object pointer.
     def tearDown(self):
         super(TestVxlan, self).tearDown()
         self.cli(2, "show bridge-domain 1 detail")

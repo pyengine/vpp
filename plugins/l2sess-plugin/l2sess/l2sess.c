69ef079bf8edd0bcb9c3331adc3c865cb2f74022
Andrew Yourtchenko <ayourtch@gmail.com>
2016-11-29 21:16:38 +0000

    indent the l2sess plugin

--- plugins/l2sess-plugin/l2sess/l2sess.c
+++ plugins/l2sess-plugin/l2sess/l2sess.c
@@ -92,11 +92,11 @@ _(L2SESS_BIND_TABLES, l2sess_bind_tables)
 
 clib_error_t *
 vlib_plugin_register (vlib_main_t * vm, vnet_plugin_handoff_t * h,
-                      int from_early_init)
+		      int from_early_init)
 {
-  l2sess_main_t * sm = &l2sess_main;
-  clib_error_t * error = 0;
-  memset(sm, 0, sizeof(*sm));
+  l2sess_main_t *sm = &l2sess_main;
+  clib_error_t *error = 0;
+  memset (sm, 0, sizeof (*sm));
 
   sm->vlib_main = vm;
   sm->vnet_main = h->vnet_main;
@@ -131,16 +131,21 @@ foreach_l2sess_node
 }
 */
 
-void l2sess_init_next_features_input(vlib_main_t *vm, l2sess_main_t * sm) {
+void
+l2sess_init_next_features_input (vlib_main_t * vm, l2sess_main_t * sm)
+{
 #define _(node_name, node_var, is_out, is_ip6, is_track) \
   if (!is_out) feat_bitmap_init_next_nodes(vm, node_var.index, L2INPUT_N_FEAT,  l2input_get_feat_names (), sm->node_var ## _input_next_node_index);
-foreach_l2sess_node
+  foreach_l2sess_node
 #undef _
 }
 
-void l2sess_add_our_next_nodes(vlib_main_t *vm, l2sess_main_t * sm, u8 *prev_node_name, int add_output_nodes) {
+void
+l2sess_add_our_next_nodes (vlib_main_t * vm, l2sess_main_t * sm,
+			   u8 * prev_node_name, int add_output_nodes)
+{
   vlib_node_t *n;
-  n = vlib_get_node_by_name(vm, prev_node_name);
+  n = vlib_get_node_by_name (vm, prev_node_name);
 #define _(node_name, node_var, is_out, is_ip6, is_track) \
   if (is_out == add_output_nodes) { \
     u32 idx = vlib_node_add_next_with_slot(vm, n->index, node_var.index, ~0); \
@@ -148,120 +153,142 @@ void l2sess_add_our_next_nodes(vlib_main_t *vm, l2sess_main_t * sm, u8 *prev_nod
       sm->next_slot_track_node_by_is_ip6_is_out[is_ip6][is_out] = idx; \
     } \
   }
-foreach_l2sess_node
+  foreach_l2sess_node
 #undef _
 }
 
-void l2sess_setup_nodes(void)
+void
+l2sess_setup_nodes (void)
 {
   vlib_main_t *vm = vlib_get_main ();
-  l2sess_main_t * sm = &l2sess_main;
+  l2sess_main_t *sm = &l2sess_main;
 
-  l2sess_init_next_features_input(vm, sm);
+  l2sess_init_next_features_input (vm, sm);
 
-  l2sess_add_our_next_nodes(vm, sm, (u8 *) "l2-input-classify", 0);
-  l2sess_add_our_next_nodes(vm, sm, (u8 *) "l2-output-classify", 1);
+  l2sess_add_our_next_nodes (vm, sm, (u8 *) "l2-input-classify", 0);
+  l2sess_add_our_next_nodes (vm, sm, (u8 *) "l2-output-classify", 1);
 
 }
 
 
 
 static clib_error_t *
-show_l2sess_bind_command_fn(vlib_main_t * vm,
-                                   unformat_input_t * input,
-                                   vlib_cli_command_t * cmd)
+show_l2sess_bind_command_fn (vlib_main_t * vm,
+			     unformat_input_t * input,
+			     vlib_cli_command_t * cmd)
 {
-  l2sess_main_t * sm = &l2sess_main;
+  l2sess_main_t *sm = &l2sess_main;
   u32 fwd_table_index = ~0;
 
-  if (unformat(input, "%d", &fwd_table_index)) {
-    if(fwd_table_index < vec_len(sm->fwd_to_rev_by_table_index)) {
-      vlib_cli_output(vm, "l2sess bind %d %d\n", fwd_table_index, sm->fwd_to_rev_by_table_index[fwd_table_index]);
-    } else {
-      vlib_cli_output(vm, "l2sess bind %d -1\n");
+  if (unformat (input, "%d", &fwd_table_index))
+    {
+      if (fwd_table_index < vec_len (sm->fwd_to_rev_by_table_index))
+	{
+	  vlib_cli_output (vm, "l2sess bind %d %d\n", fwd_table_index,
+			   sm->fwd_to_rev_by_table_index[fwd_table_index]);
+	}
+      else
+	{
+	  vlib_cli_output (vm, "l2sess bind %d -1\n");
+	}
     }
-  } 
-  for(fwd_table_index=0; fwd_table_index < vec_len(sm->fwd_to_rev_by_table_index); fwd_table_index++) {
-    vlib_cli_output(vm, "l2sess bind %d %d\n", fwd_table_index, sm->fwd_to_rev_by_table_index[fwd_table_index]);
-  }
+  for (fwd_table_index = 0;
+       fwd_table_index < vec_len (sm->fwd_to_rev_by_table_index);
+       fwd_table_index++)
+    {
+      vlib_cli_output (vm, "l2sess bind %d %d\n", fwd_table_index,
+		       sm->fwd_to_rev_by_table_index[fwd_table_index]);
+    }
   return 0;
 }
 
-VLIB_CLI_COMMAND (show_l2sess_bind_command, static) = {
-    .path = "show l2sess bind",
-    .short_help = "show l2sess bind [<table-index>]",
-    .function = show_l2sess_bind_command_fn,
-};
+VLIB_CLI_COMMAND (show_l2sess_bind_command, static) =
+{
+.path = "show l2sess bind",.short_help =
+    "show l2sess bind [<table-index>]",.function =
+    show_l2sess_bind_command_fn,};
 
 /* Action function shared between message handler and debug CLI */
 
-int l2sess_bind_tables (l2sess_main_t * sm, u32 fwd_table_index, u32 rev_table_index, u8 bind_type)
+int
+l2sess_bind_tables (l2sess_main_t * sm, u32 fwd_table_index,
+		    u32 rev_table_index, u8 bind_type)
 {
   int rv = 0;
   u32 old_fwd_table_index;
   u32 old_rev_table_index;
 
-  if (~0 == fwd_table_index) {
-    /* The first argument MUST be a valid table index */
-    return -1;
-  }
-  vec_validate_init_empty(sm->fwd_to_rev_by_table_index, fwd_table_index, ~0);
-
-  if (~0 != rev_table_index) {
-    vec_validate_init_empty(sm->fwd_to_rev_by_table_index, rev_table_index, ~0);
-
-    /* Both tables might already be potential members of other two bindings, break that first. */
-    old_fwd_table_index = sm->fwd_to_rev_by_table_index[rev_table_index];
-    old_rev_table_index = sm->fwd_to_rev_by_table_index[fwd_table_index];
-
-    /* The validity of old_fwd_table_index and old_rev_table_index was verified when someone assigned them */
-    if(~0 != old_fwd_table_index) {
-      sm->fwd_to_rev_by_table_index[old_fwd_table_index] = ~0;
+  if (~0 == fwd_table_index)
+    {
+      /* The first argument MUST be a valid table index */
+      return -1;
     }
-    if(~0 != old_rev_table_index) {
-      sm->fwd_to_rev_by_table_index[old_rev_table_index] = ~0;
+  vec_validate_init_empty (sm->fwd_to_rev_by_table_index, fwd_table_index,
+			   ~0);
+
+  if (~0 != rev_table_index)
+    {
+      vec_validate_init_empty (sm->fwd_to_rev_by_table_index, rev_table_index,
+			       ~0);
+
+      /* Both tables might already be potential members of other two bindings, break that first. */
+      old_fwd_table_index = sm->fwd_to_rev_by_table_index[rev_table_index];
+      old_rev_table_index = sm->fwd_to_rev_by_table_index[fwd_table_index];
+
+      /* The validity of old_fwd_table_index and old_rev_table_index was verified when someone assigned them */
+      if (~0 != old_fwd_table_index)
+	{
+	  sm->fwd_to_rev_by_table_index[old_fwd_table_index] = ~0;
+	}
+      if (~0 != old_rev_table_index)
+	{
+	  sm->fwd_to_rev_by_table_index[old_rev_table_index] = ~0;
+	}
+
+      sm->fwd_to_rev_by_table_index[fwd_table_index] = rev_table_index;
+      sm->fwd_to_rev_by_table_index[rev_table_index] = fwd_table_index;
     }
-    
-    sm->fwd_to_rev_by_table_index[fwd_table_index] = rev_table_index;
-    sm->fwd_to_rev_by_table_index[rev_table_index] = fwd_table_index;
-  } else {
-    /* Break up the binding */
-    rev_table_index = sm->fwd_to_rev_by_table_index[fwd_table_index];
-    
-    sm->fwd_to_rev_by_table_index[fwd_table_index] = ~0;
-    sm->fwd_to_rev_by_table_index[rev_table_index] = ~0;
-  }
+  else
+    {
+      /* Break up the binding */
+      rev_table_index = sm->fwd_to_rev_by_table_index[fwd_table_index];
+
+      sm->fwd_to_rev_by_table_index[fwd_table_index] = ~0;
+      sm->fwd_to_rev_by_table_index[rev_table_index] = ~0;
+    }
   return rv;
 }
 
 static clib_error_t *
 macswap_enable_disable_command_fn (vlib_main_t * vm,
-                                   unformat_input_t * input,
-                                   vlib_cli_command_t * cmd)
+				   unformat_input_t * input,
+				   vlib_cli_command_t * cmd)
 {
-  l2sess_main_t * sm = &l2sess_main;
+  l2sess_main_t *sm = &l2sess_main;
   int rv;
   u32 fwd_table_index;
   u32 rev_table_index;
   u8 bind_type = 0;
 
-  if (!unformat(input, "%d", &fwd_table_index)) {
-    return clib_error_return(0, "forward table index required");
-  }
-  if (!unformat(input, "%d", &rev_table_index)) {
-    return clib_error_return(0, "reverse table index required");
-  }
+  if (!unformat (input, "%d", &fwd_table_index))
+    {
+      return clib_error_return (0, "forward table index required");
+    }
+  if (!unformat (input, "%d", &rev_table_index))
+    {
+      return clib_error_return (0, "reverse table index required");
+    }
 
   rv = l2sess_bind_tables (sm, fwd_table_index, rev_table_index, bind_type);
 
-  switch(rv) {
-  case 0:
-    break;
+  switch (rv)
+    {
+    case 0:
+      break;
 
-  default:
-    return clib_error_return (0, "l2sess_bind_tables returned %d",
-                              rv);
-  }
+    default:
+      return clib_error_return (0, "l2sess_bind_tables returned %d", rv);
+    }
   return 0;
 }
 
@@ -273,68 +300,111 @@ VLIB_CLI_COMMAND (l2sess_bind_command, static) = {
 };
 /* *INDENT-ON* */
 
-static char *get_l4_proto_str(int is_ip6, uint8_t l4_proto) {
-  switch (l4_proto) {
-    case 6:  return "tcp";
-    case 17: return "udp";
-    case 1:  return "icmp";
-    case 58: return "icmp6";
-    default: return "<?l4-unknown?>";
-  }
+static char *
+get_l4_proto_str (int is_ip6, uint8_t l4_proto)
+{
+  switch (l4_proto)
+    {
+    case 6:
+      return "tcp";
+    case 17:
+      return "udp";
+    case 1:
+      return "icmp";
+    case 58:
+      return "icmp6";
+    default:
+      return "<?l4-unknown?>";
+    }
 }
 
 static clib_error_t *
-l2sess_show_command_fn(vlib_main_t * vm,
-                                   unformat_input_t * input,
-                                   vlib_cli_command_t * cmd)
+l2sess_show_command_fn (vlib_main_t * vm,
+			unformat_input_t * input, vlib_cli_command_t * cmd)
 {
-  l2sess_main_t * sm = &l2sess_main;
+  l2sess_main_t *sm = &l2sess_main;
   clib_time_t *ct = &vm->clib_time;
   l2s_session_t *s;
   u64 now = clib_cpu_time_now ();
 
-  vlib_cli_output(vm, "Timing wheel info: \n%U", format_timing_wheel, &sm->timing_wheel, 255);
+  vlib_cli_output (vm, "Timing wheel info: \n%U", format_timing_wheel,
+		   &sm->timing_wheel, 255);
 
-  pool_foreach(s, sm->sessions,
-  ({
-    f64 ctime = (now - s->create_time) * ct->seconds_per_clock;
-    f64 atime0 = (now - s->side[0].active_time) * ct->seconds_per_clock;
-    f64 atime1 = (now - s->side[1].active_time) * ct->seconds_per_clock;
-
+  pool_foreach (s, sm->sessions, (
+				   {
+				   f64 ctime =
+				   (now -
+				    s->create_time) * ct->seconds_per_clock;
+				   f64 atime0 =
+				   (now -
+				    s->side[0].active_time) *
+				   ct->seconds_per_clock;
+				   f64 atime1 =
+				   (now -
+				    s->side[1].active_time) *
+				   ct->seconds_per_clock;
 /*
     f64 ctime = (s->create_time - vm->cpu_time_main_loop_start) * ct->seconds_per_clock;
     f64 atime0 = (s->side[0].active_time - vm->cpu_time_main_loop_start) * ct->seconds_per_clock;
     f64 atime1 = (s->side[1].active_time - vm->cpu_time_main_loop_start) * ct->seconds_per_clock;
 */
-    u8 *out0 = format(0, "%5d: create time: %U pkts/bytes/active time: [ %ld %ld %U : %ld %ld %U ]\n", (s - sm->sessions),
-			format_time_interval,  "h:m:s:u", ctime,
-			s->side[0].n_packets, s->side[0].n_bytes,
-			format_time_interval,  "h:m:s:u", atime0,
-			s->side[1].n_packets, s->side[1].n_bytes,
-			format_time_interval,  "h:m:s:u", atime1);
-    u8 *out1 = 0;
-    if (s->is_ip6) {
-      out1 = format(0, "%s %U :%u <-> %U :%u", get_l4_proto_str(s->is_ip6, s->l4_proto), format_ip6_address, &s->side[0].addr.ip6, s->side[0].port, format_ip6_address, &s->side[1].addr.ip6, s->side[1].port);
-    } else {
-      out1 = format(0, "%s %U :%u <-> %U :%u", get_l4_proto_str(s->is_ip6, s->l4_proto), format_ip4_address, &s->side[0].addr.ip4, s->side[0].port, format_ip4_address, &s->side[1].addr.ip4, s->side[1].port);
-    }
-    vlib_cli_output(vm, "%s       %s", out0, out1);
-    vec_free(out0);
-    vec_free(out1);
-  }));
+				   u8 * out0 =
+				   format (0,
+					   "%5d: create time: %U pkts/bytes/active time: [ %ld %ld %U : %ld %ld %U ]\n",
+					   (s - sm->sessions),
+					   format_time_interval, "h:m:s:u",
+					   ctime, s->side[0].n_packets,
+					   s->side[0].n_bytes,
+					   format_time_interval, "h:m:s:u",
+					   atime0, s->side[1].n_packets,
+					   s->side[1].n_bytes,
+					   format_time_interval, "h:m:s:u",
+					   atime1); u8 * out1 = 0;
+				   if (s->is_ip6)
+				   {
+				   out1 =
+				   format (0, "%s %U :%u <-> %U :%u",
+					   get_l4_proto_str (s->is_ip6,
+							     s->l4_proto),
+					   format_ip6_address,
+					   &s->side[0].addr.ip6,
+					   s->side[0].port,
+					   format_ip6_address,
+					   &s->side[1].addr.ip6,
+					   s->side[1].port);}
+				   else
+				   {
+				   out1 =
+				   format (0, "%s %U :%u <-> %U :%u",
+					   get_l4_proto_str (s->is_ip6,
+							     s->l4_proto),
+					   format_ip4_address,
+					   &s->side[0].addr.ip4,
+					   s->side[0].port,
+					   format_ip4_address,
+					   &s->side[1].addr.ip4,
+					   s->side[1].port);}
+				   vlib_cli_output (vm, "%s       %s", out0,
+						    out1); vec_free (out0);
+				   vec_free (out1);}
+		));
   return 0;
 }
 
 static clib_error_t *
-l2sess_show_count_command_fn(vlib_main_t * vm,
-                                   unformat_input_t * input,
-                                   vlib_cli_command_t * cmd)
+l2sess_show_count_command_fn (vlib_main_t * vm,
+			      unformat_input_t * input,
+			      vlib_cli_command_t * cmd)
 {
-  l2sess_main_t * sm = &l2sess_main;
+  l2sess_main_t *sm = &l2sess_main;
 
-  vlib_cli_output(vm, "Timing wheel info: \n%U", format_timing_wheel, &sm->timing_wheel, 255);
-  vlib_cli_output(vm, "session pool len: %d, pool elts: %d", pool_len(sm->sessions), pool_elts(sm->sessions));
-  vlib_cli_output(vm, "attempted to delete sessions which were already free: %d", sm->counter_attempted_delete_free_session);
+  vlib_cli_output (vm, "Timing wheel info: \n%U", format_timing_wheel,
+		   &sm->timing_wheel, 255);
+  vlib_cli_output (vm, "session pool len: %d, pool elts: %d",
+		   pool_len (sm->sessions), pool_elts (sm->sessions));
+  vlib_cli_output (vm,
+		   "attempted to delete sessions which were already free: %d",
+		   sm->counter_attempted_delete_free_session);
   return 0;
 }
 

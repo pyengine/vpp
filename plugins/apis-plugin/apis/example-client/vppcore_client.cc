8def238cd7278332d1916b85614b5dd2f5732cbf
cisco <cisco@pumbba.cisco.com>
2016-12-08 11:17:46 +0530

    Added example performance test cpp and py
    Signed-off-by: cisco <cisco@pumbba.cisco.com>

--- plugins/apis-plugin/apis/example-client/vppcore_client.cc
+++ plugins/apis-plugin/apis/example-client/vppcore_client.cc
@@ -1,94 +1,179 @@
 #include <iostream>
 #include <memory>
 #include <string>
-
+#include <sys/time.h>
 #include <grpc++/grpc++.h>
-
+#include <math.h>
 #include "../example-client/vppcore.grpc.pb.h"
 
-using grpc::Channel;
-using grpc::ClientContext;
-using grpc::Status;
-using vppcore::vppClientContext;
-using vppcore::ShowVersionReply;
-using vppcore::vpp;
+using
+  grpc::Channel;
+using
+  grpc::ClientContext;
+using
+  grpc::Status;
+using
+  vppcore::vppClientContext;
+using
+  vppcore::ShowVersionReply;
+using
+  vppcore::vpp;
 
-class vppClient {
- public:
-  vppClient(std::shared_ptr<Channel> channel)
-      : stub_(vpp::NewStub(channel)) {}
+class
+  vppClient
+{
+public:
+  vppClient (std::shared_ptr < Channel > channel):
+  stub_ (vpp::NewStub (channel))
+  {
+  }
 
   // Assembles the client's payload, sends it and presents the response back
   // from the server.
-  std::string ShowVersion(const std::string& user) {
+  std::string
+  PerfShowVersion (const std::string & user, float number)
+  {
     // Data we are sending to the server.
     vppcore::vppClientContext request;
-    request.set_name(user);
+    request.set_name (user);
 
     // Container for the data we expect from the server.
-    ShowVersionReply reply;
-    ClientContext context;
+    ShowVersionReply
+      reply;
+    struct timeval
+      start,
+      end;
+    Status
+      status;
 
-    // The actual RPC.
-    Status status = stub_->ShowVersion(&context, request, &reply);
-
-    // Act upon its status.
-    if (status.ok()) {
-    	std::cout << status.error_code() << ": " << status.error_message()
-    			<< std::endl;
-    	std::cout << reply.program() << ": " << reply.version() << ": "
-    			<< reply.build_date() << ": "<< reply.build_directory()
-				<< std::endl;
-      return reply.version();
-    } else {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      return "RPC failed";
-    }
+    gettimeofday (&start, NULL);
+    for (int i = 0; i < number; i++)
+      {
+	ClientContext
+	  context;
+	status = stub_->ShowVersion (&context, request, &reply);
+      }
+    gettimeofday (&end, NULL);
+    long long
+      time = (end.tv_sec * (unsigned int) 1e6 + end.tv_usec) -
+      (start.tv_sec * (unsigned int) 1e6 + start.tv_usec);
+    if (status.ok ())
+      {
+	std::cout << "time taken = " << time << "µs" << std::endl;
+	std::cout << "cps = " << pow (10, 6) * number / time << std::endl;
+	std::cout << status.error_code () << ": " << status.error_message ()
+	  << std::endl;
+	std::cout << reply.program () << ": " << reply.version () << ": "
+	  << reply.build_date () << ": " << reply.build_directory ()
+	  << std::endl;
+	return reply.version ();
+      }
+    else
+      {
+	std::cout << status.error_code () << ": " << status.error_message ()
+	  << std::endl;
+	return "RPC failed";
+      }
   }
 
-  std::string runCommand(const std::string& command) {
+
+  // Assembles the client's payload, sends it and presents the response back
+  // from the server.
+  std::string ShowVersion (const std::string & user)
+  {
+    // Data we are sending to the server.
+    vppcore::vppClientContext request;
+    request.set_name (user);
+
+    // Container for the data we expect from the server.
+    ShowVersionReply
+      reply;
+    struct timeval
+      start,
+      end;
+    Status
+      status;
+    ClientContext
+      context;
+
+    status = stub_->ShowVersion (&context, request, &reply);
+
+    if (status.ok ())
+      {
+	std::cout << status.error_code () << ": " << status.error_message ()
+	  << std::endl;
+	std::cout << reply.program () << ": " << reply.version () << ": "
+	  << reply.build_date () << ": " << reply.build_directory ()
+	  << std::endl;
+	return reply.version ();
+      }
+    else
+      {
+	std::cout << status.error_code () << ": " << status.error_message ()
+	  << std::endl;
+	return "RPC failed";
+      }
+  }
+
+  std::string runCommand (const std::string & command)
+  {
     // Data we are sending to the server.
     vppcore::vppCommand request;
-    request.set_command(command);
+    request.set_command (command);
 
     // Container for the data we expect from the server.
     vppcore::vppCommandOutput reply;
-    ClientContext context;
+    ClientContext
+      context;
 
-    // The actual RPC.
-    Status status = stub_->ExecCli(&context, request, &reply);
+    Status
+      status = stub_->ExecCli (&context, request, &reply);
 
-    // Act upon its status.
-    if (status.ok()) {
-    	std::cout << status.error_code() << ": " << status.error_message()
-    			<< std::endl;
-    	std::cout << reply.cliout()
-				<< std::endl;
-      return reply.cliout();
-    } else {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      return "RPC failed";
-    }
+    if (status.ok ())
+      {
+	std::cout << status.error_code () << ": " << status.error_message ()
+	  << std::endl;
+	std::cout << reply.cliout () << std::endl;
+	return reply.cliout ();
+      }
+    else
+      {
+	std::cout << status.error_code () << ": " << status.error_message ()
+	  << std::endl;
+	return "RPC failed";
+      }
   }
 
 
- private:
-  std::unique_ptr<vpp::Stub> stub_;
+private:
+  std::unique_ptr < vpp::Stub > stub_;
 };
 
-int main(int argc, char** argv) {
+int
+main (int argc, char **argv)
+{
   // Instantiate the client. It requires a channel, out of which the actual RPCs
   // are created. This channel models a connection to an endpoint (in this case,
   // localhost at port 50051). We indicate that the channel isn't authenticated
   // (use of InsecureChannelCredentials()).
-  vppClient vpp(grpc::CreateChannel(
-      "localhost:50051", grpc::InsecureChannelCredentials()));
-  std::string user("cppvpp");
-  std::string reply = vpp.ShowVersion(user);
+  std::string target = "localhost:50051";
+  int
+    number_of_requests = 0;
+
+  if (argc > 1)
+    number_of_requests = atoi (argv[1]);
+  if (argc > 2)
+    target = argv[2];
+
+  vppClient
+  vpp (grpc::CreateChannel (target, grpc::InsecureChannelCredentials ()));
+  std::string user ("cppvpp");
+  std::string reply = vpp.ShowVersion (user);
   std::cout << "Version received: " << reply << std::endl;
-  std::string show_interface_out = vpp.runCommand("show interface");
-  std::cout << "show interface: " << show_interface_out << std::endl;
+
+  if (number_of_requests > 0)
+    vpp.PerfShowVersion (user, number_of_requests);
+  //std::string show_interface_out = vpp.runCommand("show interface");
+  //std::cout << "show interface: " << show_interface_out << std::endl;
   return 0;
 }
---
+++ plugins/apis-plugin/apis/example-client/vppcore_version_perftest.py
@@ -0,0 +1,46 @@
+"" "The Python implementation of the GRPC vppcore client." ""
+from __future__ import print_function
+import sys, getopt
+import grpc
+import vppcore_pb2
+import timeit
+def test_setup (host, port):
+   global stub
+   target = '{0}:{1}'.format (host, port)
+   print ("connecting to ", target)
+   channel = grpc.insecure_channel (target)
+   stub = vppcore_pb2.vppStub (channel)
+
+def test_showversion ():
+   global stub
+   t = stub.ShowVersion (vppcore_pb2.vppClientContext (name = 'perfclient'))
+
+def run (argv):
+   host = 'localhost'
+   port = 50051
+   number_of_requests = 15000
+   try:
+     opts, args = getopt.getopt (argv[1:], "ht:p:n:",["host=", "port=", "number_of_calls="])
+   except getopt.GetoptError:
+     print (argv[0], ' -t <target vpp host> -p <port> -n <number of calls>')
+     sys.exit (2)
+     for opt,arg in opts:
+        if opt == '-h':
+           print (argv[0], ' -t <target vpp host> -p <port> -n <number of calls>')
+           sys.exit ()
+        elif opt in ("-t", "--host"):
+           host = arg
+        elif opt in ("-p", "--port"):
+           port = int (arg)
+        elif opt in ("-n", "--number_of_call"):
+           number_of_requests = int (arg)
+   test_setup(host, port)
+   print ("Calling show verstion on {0}:{1} ({2} times)".format(host, port, number_of_requests))
+   time_taken = timeit.timeit ('test_showversion()', number =
+			      number_of_requests, setup =
+			      "from __main__ import test_showversion")
+   print ("Time taken {0} seconds".format (time_taken))
+   print ("{0} calls per second".format (number_of_requests / time_taken))
+
+if __name__ == '__main__':
+   run (sys.argv)
